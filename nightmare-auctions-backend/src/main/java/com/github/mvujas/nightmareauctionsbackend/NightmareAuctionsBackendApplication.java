f4b8f1b0c1a3c42eb19c505c887e8b9c3174d403
Milos <18309829+mvujas@users.noreply.github.com>
2020-01-17 19:04:30 +0100

    Empty Spring Project added

---
+++ nightmare-auctions-backend/src/main/java/com/github/mvujas/nightmareauctionsbackend/NightmareAuctionsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.github.mvujas.nightmareauctionsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NightmareAuctionsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NightmareAuctionsBackendApplication.class, args);
+	}
+
+}
---
+++ nightmare-auctions-backend/src/test/java/com/github/mvujas/nightmareauctionsbackend/NightmareAuctionsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.mvujas.nightmareauctionsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NightmareAuctionsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
