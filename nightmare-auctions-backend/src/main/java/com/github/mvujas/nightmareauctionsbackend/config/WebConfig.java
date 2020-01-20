9fa58a2799443422ebc0b182c2da43d08d06106f
Milos <18309829+mvujas@users.noreply.github.com>
2020-01-20 22:19:14 +0100

    Authorization added to exposed headers, all headers, methods and origins are now allowed

--- nightmare-auctions-backend/src/main/java/com/github/mvujas/nightmareauctionsbackend/config/WebConfig.java
+++ nightmare-auctions-backend/src/main/java/com/github/mvujas/nightmareauctionsbackend/config/WebConfig.java
@@ -6,8 +6,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("*");
-    }
+	
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry
+			.addMapping("/**")
+			.allowedHeaders("*")
+			.allowedMethods("*")
+			.allowedOrigins("*")
+			.exposedHeaders("Authorization");
+	}
 }
