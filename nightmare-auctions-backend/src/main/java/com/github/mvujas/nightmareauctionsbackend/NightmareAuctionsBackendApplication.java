1b79474dd40fb3f0d9f1682bc5fff3c5f5457b09
Milos <18309829+mvujas@users.noreply.github.com>
2020-01-22 11:27:42 +0100

    Enabled global method security

--- nightmare-auctions-backend/src/main/java/com/github/mvujas/nightmareauctionsbackend/NightmareAuctionsBackendApplication.java
+++ nightmare-auctions-backend/src/main/java/com/github/mvujas/nightmareauctionsbackend/NightmareAuctionsBackendApplication.java
@@ -2,8 +2,12 @@ package com.github.mvujas.nightmareauctionsbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 @SpringBootApplication
+@EnableGlobalMethodSecurity(
+		securedEnabled = true,
+		prePostEnabled = true)
 public class NightmareAuctionsBackendApplication {
 
 	public static void main(String[] args) {
